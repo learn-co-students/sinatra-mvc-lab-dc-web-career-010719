81df62ad3ef76b3760eceb041e97a7975989822d
shannon-nabors <shannon.nabors@gmail.com>
2019-01-28 18:46:48 -0500

    done

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+
+class PigLatinizer
+
+  def piglatinize(string)
+    vowels = %w(a e i o u)
+    latinized = []
+    string.split(" ").each do |word|
+      if vowels.include?(word[0].downcase)
+        latinized << (word + "way")
+      else
+        indices = vowels.map {|v| word.index(v)}.select {|e| e.class == Fixnum }
+        latinized << (word[(indices.min)..-1] + word[0..(indices.min-1)] + "ay")
+      end
+    end
+    latinized.join(" ")
+  end
+
+end
